43858be30e55a03db6c977c895619eaf4e2df107
KarolGuerrero <stefaniortiz2905@gmail.com>
2024-09-11 21:43:01 -0500

    consolidar todos los archivos en uno solo y modificar la salida del programa

--- AnalizadorLexico.py
+++ AnalizadorLexico.py
@@ -1,7 +1,222 @@
-from tokens.tokens import character_special,word_reserved,methods,data_types
-from tokens.token_class import Token
-from tokens.error import Error
-from tokens.identifier_class import Identifier
+
+character_special = {
+    ')': 'tk_par_der',
+    '(': 'tk_par_izq',
+    ']': 'tk_sqb_der',
+    '[': 'tk_sqb_izq',
+    ':': 'tk_dos_puntos',
+    ',': 'tk_comma',
+    ';': 'tk_punto_comma',
+    '+': 'tk_plus',
+    '-': 'tk_minus',
+    '*': 'tk_star',
+    '/': 'tk_slash',
+    '|': 'tk_vbar',
+    '&': 'tk_amper',
+    '<': 'tk_less',
+    '>': 'tk_greater',
+    '=': 'tk_asing',
+    '.': 'tk_punto',
+    '%': 'tk_percent',
+    '}': 'tk_brace_der',
+    '{': 'tk_brace_izq',
+    '==': 'tk_equal',
+    '!=': 'tk_no_equal',
+    '<=': 'tk_less_equal',
+    '>=': 'tk_greate_equal',
+    '^': 'tk_circumflex',
+    '<<': 'tk_shift_izq',
+    '>>': 'tk_shift_der',
+    '**': 'tk_double_star',
+    '+=': 'tk_plus_equal',
+    '-=': 'tk_mine_equal',
+    '*=': 'tk_star_equal',
+    '/=': 'tk_slash_equal',
+    '%=': 'tk_percent_equal',
+    '&=': 'tk_amper_equal',
+    '|=': 'tk_vabar_equal',
+    '^=': 'tk_circum_flex_equal',
+    '<<=': 'tk_shift_equal_izq',
+    '>>=': 'tk_shift_equal_der',
+    '**=': 'tk_double_start_equal',
+    '//': 'tk_double_slash',
+    '//=': 'tk_double_slash_equal',
+    '@': 'tk_at',
+    '@=': 'tk_at_equal',
+    '->': 'tk_ejecuta',
+    '...': 'tk_ellipsis',
+    ':=': 'tk_dos_puntos_equal'
+}
+#reserved words
+word_reserved={
+    'open':'open',
+    'False': 'False',
+    'None': 'None',
+    'True': 'True',
+    'and': 'and',
+    'as': 'as',
+    'assert': 'assert',
+    'async': 'async',
+    'await': 'await',
+    'break': 'break',
+    'class': 'class',
+    'continue': 'continue',
+    'def': 'def',
+    'del': 'del',
+    'elif': 'elif',
+    'else': 'else',
+    'except': 'except',
+    'finally': 'finally',
+    'for': 'for',
+    'from': 'from',
+    'global': 'global',
+    'if': 'if',
+    'import': 'import',
+    'in': 'in',
+    'is': 'is',
+    'lambda': 'lambda',
+    'nonlocal': 'nonlocal',
+    'not': 'not',
+    'or': 'or',
+    'pass': 'pass',
+    'raise': 'raise',
+    'return': 'return',
+    'try': 'try',
+    'while': 'while',
+    'with': 'with',
+    'yield': 'yield',
+    'object': 'object',
+    'self':'self',
+    'print': 'print',
+    'input':'input',
+}
+#metodos
+methods={
+    'append': 'append',
+    'extend': 'extend',
+    'insert': 'insert',
+    'reverse': 'reverse',
+    'sort': 'sort',
+    'fromkeys': 'fromkeys',
+    'get': 'get',
+    'items': 'items',
+    'keys': 'keys',
+    'popitem': 'popitem',
+    'setdefault': 'setdefault',
+    'values': 'values',
+    'add': 'add',
+    'clear': 'clear',
+    'copy': 'copy',
+    'difference': 'difference',
+    'difference_update': 'difference_update',
+    'discard': 'discard',
+    'intersection': 'intersection',
+    'intersection_update': 'intersection_update',
+    'isdisjoint': 'isdisjoint',
+    'issubset': 'issubset',
+    'issuperset': 'issuperset',
+    'pop': 'pop',
+    'remove': 'remove',
+    'symmetric_difference': 'symmetric_difference',
+    'symmetric_difference_update': 'symmetric_difference_update',
+    'union': 'union',
+    'update': 'update',
+    'capitalize': 'capitalize',
+    'casefold': 'casefold',
+    'center': 'center',
+    'count': 'count',
+    'encode': 'encode',
+    'endswith': 'endswith',
+    'expandtabs': 'expandtabs',
+    'find': 'find',
+    'format': 'format',
+    'format_map': 'format_map',
+    'index': 'index',
+    'isalnum': 'isalnum',
+    'isalpha': 'isalpha',
+    'isascii': 'isascii',
+    'isdecimal': 'isdecimal',
+    'isdigit': 'isdigit',
+    'isidentifier': 'isidentifier',
+    'islower': 'islower',
+    'isnumeric': 'isnumeric',
+    'isprintable': 'isprintable',
+    'isspace': 'isspace',
+    'istitle': 'istitle',
+    'isupper': 'isupper',
+    'join': 'join',
+    'ljust': 'ljust',
+    'lower': 'lower',
+    'lstrip': 'lstrip',
+    'maketrans': 'maketrans',
+    'partition': 'partition',
+    'replace': 'replace',
+    'rfind': 'rfind',
+    'rindex': 'rindex',
+    'rjust': 'rjust',
+    'rpartition': 'rpartition',
+    'rsplit': 'rsplit',
+    'rstrip': 'rstrip',
+    'split': 'split',
+    'splitlines': 'splitlines',
+    'startswith': 'startswith',
+    'strip': 'strip',
+    'swapcase': 'swapcase',
+    'title': 'title',
+    'translate': 'translate',
+    'upper': 'upper',
+    'zfill': 'zfill',
+    'bit_length': 'bit_length',
+    'to_bytes': 'to_bytes',
+    'from_bytes': 'from_bytes',
+    'is_integer': 'is_integer',
+    'hex': 'hex',
+    'conjugate': 'conjugate',
+    'real': 'real',
+    'imag': 'imag'
+}
+data_types = {
+    "int": "int",
+    "float": "float",
+    "complex": "complex",
+    "bool": "bool",
+    "str": "str",
+    "list": "list",
+    "tuple": "tuple",
+    "set": "set",
+    "dict": "dict",
+    "NoneT": "None",
+    "bytes": "bytes",
+    "bytearray": "bytearray",
+    "range": "range",
+    "frozenset": "frozenset",
+}
+
+class Token:
+    def __init__(self, nombre, fila, columna):
+        self.nombre = nombre
+        self.fila = fila
+        self.columna = columna
+    def __str__(self):
+        return f"<{self.nombre}, {self.fila}, {self.columna}>"
+    
+class Identifier:
+    def __init__(self, nombre, fila, columna):
+        self.nombre = nombre
+        self.fila = fila
+        self.columna = columna
+
+    def __str__(self):
+        return f"<id,{self.nombre}, {self.fila}, {self.columna}>"
+    
+class Error:
+    def __init__(self, fila, columna, mensaje=">>>Error léxico"):
+        self.fila = fila
+        self.columna = columna
+        self.mensaje = mensaje
+    def __str__(self):
+        return f"{self.mensaje} (línea: {self.fila}, posición: {self.columna})"
+    
 def read_file(file_name):
     lines = []
     with open(file_name, 'r', encoding='UTF-8') as file:
@@ -128,3 +343,18 @@ def clasificadorDeTokens(tokensFinales):
                 tokenClasificado.append(objeto)
                 return tokenClasificado
     return tokenClasificado
+
+path = "ejemplo_prueba.txt"
+def run(path):
+    texto=read_file(path)
+    tokens = generadorTokensSinProcesar(texto)
+    salida = clasificadorDeTokens(tokens)
+    
+    output_file = 'analisis_tokens.txt'
+    
+    with open(output_file, 'w', encoding='UTF-8') as file:
+        for token in salida:
+            file.write(str(token) + '\n')
+
+if __name__ == '__main__':
+    run(path)
--- tokens/error.py
+++
@@ -1,7 +0,0 @@
-class Error:
-    def __init__(self, fila, columna, mensaje=">>>Error léxico"):
-        self.fila = fila
-        self.columna = columna
-        self.mensaje = mensaje
-    def __str__(self):
-        return f"{self.mensaje} (línea: {self.fila}, posición: {self.columna})"
